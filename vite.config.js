20287fca9fbcdd170c91571e21bd0fc4c007e269
ebubekeyz <ebubeofforjoe@gmail.com>
2024-03-28 21:00:54 +0100

    second

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   sever: {
     proxy: {
-      '/api': 'https://trex-holding-server.com/',
+      '/api': 'https://invest-demo-server.onrender.com/',
     },
   },
   plugins: [react()],
